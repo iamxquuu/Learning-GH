07470319f1105b7ad5aeee753d3003d31b75a1b5
marcel <marcel.maciaszek@icloud.com>
2023-04-14 16:23:46 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Marcel Maciaszek on 14/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
